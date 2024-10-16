eb6a6f4acf83d0eacb40435c14c5142be1c08f7c
KONG <fwkong02@gmail.com>
2024-10-16 11:29:28 +0800

    made changes to the code such that the students are grouped in each tutorial group

--- Mini Project.py
+++ Mini Project.py
@@ -1,3 +1,5 @@
+from functions import arrangebyGender, arrangebyGPA, arrangebySchool
+
 '''
 Arrange the "records" file such that the data is stored in dictionaries in a list
 Within each dictionary, the header (first line in the file) is used for the key and the values are matched accordingly
@@ -26,113 +28,59 @@ with open('records.csv','r') as f:
         record['CGPA'] = float(record['CGPA'])
         records.append(record)
 
-def arrangebySchool(records):
-    '''
-    This function serves the purpose of arranging the unsorted list of students by School
-
-    Parameter:
-    Input: Unsorted list of dictionaries
-    Output: Lists of dictionaries in a list
-    e.g.output = [
-    'School 1': [{student 1, {student 2}, ...],  
-    'School 2': [{student 1}, {student 2}, ...], 
-    'School 3': [...], ...] 
-    '''
-    schools = {}
-    record = []
-
-    for student in records:
-        school = student['School']
-
-        if school not in schools.keys():
-            schools[school] = []
-        
-        schools[school].append(student)
-
-    return schools
-
-def arrangebyGender(records):
-    '''
-    This function serves the purpose of arranging the list of students sorted by School 
-    '''
-    males = []
-    females = []
-
-    for student in records:
-        if student['Gender'] == 'Male':
-            males.append(student)
-        else:
-            females.append(student)
+#arrange the data based on tutorial group
+new_records = {}
+for student in records:
+    tutorialGroup = student['Tutorial Group']
+    if tutorialGroup not in new_records.keys():
+        new_records[tutorialGroup] = []
     
-    record = {'Male': males,
-              'Female': females}
-    
-    return record 
-
-def arrangebyGPA(records):
-    
-    records.sort(key=lambda d: -d['CGPA'])
-    return records
-
-#test
-record_bySchoolGenderGPA = {}
-
-record_bySchool = arrangebySchool(records)
-
-for student in record_bySchool.values():
-    school = student[0]['School']
-
-    record_byGender = arrangebyGender(student)
-
-    record_byGenderGPA = {}
-    for gender in record_byGender.keys():
- 
-        record_byGenderGPA[gender] = arrangebyGPA(record_byGender[gender])
-        
-    if school not in record_bySchoolGenderGPA.keys():   
-        record_bySchoolGenderGPA[school] = record_byGenderGPA
+    new_records[tutorialGroup].append(student)
 
 
-num_groups = 120
-groups = [[] for i in range(num_groups)]
-
-# Step 4: Manually assign students to groups by alternating between subgroups
-group_index = 0
-
-for school in record_bySchoolGenderGPA:
-    for gender in record_bySchoolGenderGPA[school]:
-        for student in record_bySchoolGenderGPA[school][gender]:
-            groups[group_index].append(student)
-            group_index = (group_index + 1) % num_groups  # Rotate between groups
-
-
-import csv
 with open('new_records.csv','w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(['Tutorial Group', 'Student ID', 'School', 'Name', 'Gender', 'CPGA'])
+    
+    f.write('Tutorial Group, Student ID, School, Name, Gender, CGPA\n')
 
-    for school in record_bySchoolGenderGPA:
-        for gender in record_bySchoolGenderGPA[school]:
-            for student in record_bySchoolGenderGPA[school][gender]:
-                writer.writerow(student.values())
+    #within the tutorial group, arrange the data based on the student's school
+    for tutorialGroup in new_records:
+        studentsinSchool = arrangebySchool(new_records[tutorialGroup])
+
+        #within tutorial and school, arrange the data based on gender
+        for school in studentsinSchool:
+            studentsinSchoolGender = arrangebyGender(studentsinSchool[school])
+
+            #within tutorial, school and gender, arrange the data based on GPA
+            for gender in studentsinSchoolGender:
+                studentsinSchoolGenderGPA = arrangebyGPA(studentsinSchoolGender[gender])
+                for student in studentsinSchoolGenderGPA:
+                    student['CGPA'] = str(student['CGPA'])
+                    f.write(','.join(student.values()))
+                    f.write('\n')
+
+ 
+
+
+
+# import csv
         
-with open('groups.csv','w', newline='') as f:
-    writer = csv.writer(f)
-    writer.writerow(['Group', 'Tutorial Group', 'Student ID', 'School', 'Name', 'Gender', 'CPGA'])
-    group_index = 1
+# with open('groups.csv','w', newline='') as f:
+#     writer = csv.writer(f)
+#     writer.writerow(['Group', 'Tutorial Group', 'Student ID', 'School', 'Name', 'Gender', 'CPGA'])
+#     group_index = 1
 
-    for group in groups:
+#     for group in groups:
         
-        for student in group:
+#         for student in group:
 
-            toWrite = []
-            toWrite.append(group_index)
+#             toWrite = []
+#             toWrite.append(group_index)
 
-            for item in student.values():
-                toWrite.append(item)
+#             for item in student.values():
+#                 toWrite.append(item)
             
-            writer.writerow(toWrite)
+#             writer.writerow(toWrite)
 
-        group_index += 1
+#         group_index += 1
         
     
---
+++ functions.py
@@ -0,0 +1,50 @@
+
+def arrangebySchool(records):
+    '''
+    This function serves the purpose of arranging the unsorted list of students by School
+
+    Parameter:
+    Input: Unsorted list of dictionaries
+    Output: Lists of dictionaries in a list
+    e.g.output = [
+    'School 1': [{student 1, {student 2}, ...],  
+    'School 2': [{student 1}, {student 2}, ...], 
+    'School 3': [...], ...] 
+    '''
+    schools = {}
+    record = []
+
+    for student in records:
+        school = student['School']
+
+        if school not in schools.keys():
+            schools[school] = []
+        
+        schools[school].append(student)
+
+    return schools
+
+def arrangebyGender(records):
+    '''
+    This function serves the purpose of arranging the list of students sorted by School 
+    '''
+    males = []
+    females = []
+
+    for student in records:
+        gender = student['Gender']
+
+        if gender == 'Male':
+            males.append(student)
+        else:
+            females.append(student)
+    
+    record = {'Male': males,
+              'Female': females}
+    
+    return record 
+
+def arrangebyGPA(records):
+    
+    records.sort(key=lambda d: -d['CGPA'])
+    return records
